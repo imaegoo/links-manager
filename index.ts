82733d02b34f4a66e76f17a985a3485ced120c00
iMaeGoo <mail1st@qq.com>
2022-05-28 09:50:32 +0000

    feat: load friends

--- index.ts
+++ index.ts
@@ -1,9 +1,39 @@
 import axios from 'axios'
-import cheerio from 'cheerio'
+import { load } from 'cheerio'
+
+const domain = 'www.imaegoo.com'
+const friendPage = 'https://www.imaegoo.com/friends/'
+const friendQuerySelector = '.friend a'
 
 async function main() {
-  const friendPage = 'https://www.imaegoo.com/friends/'
-  
+  const friendPageRes = await axios({
+    url: friendPage,
+    responseType: 'text'
+  })
+  const friendPageHtml = friendPageRes.data
+  const friendPageQuery = load(friendPageHtml)
+  let friendLinks: string[] = []
+  friendPageQuery(friendQuerySelector).map((i, el) => {
+    if (el.attribs.href && el.attribs.href.startsWith('http')) {
+      friendLinks.push(el.attribs.href)
+    }
+  })
+  friendLinks = Array.from(new Set(friendLinks))
+  for (const friendLink of friendLinks) {
+    console.log(friendLink)
+  }
+}
+
+async function checkMyLink(friendLink: string) {
+  const friendPageRes = await axios({
+    url: friendLink,
+    responseType: 'text'
+  })
+  const friendPageHtml = friendPageRes.data
+  const friendPageQuery = load(friendPageHtml)
+  friendPageQuery('a').map((i, el) => {
+    // TODO
+  })
 }
 
 main()
